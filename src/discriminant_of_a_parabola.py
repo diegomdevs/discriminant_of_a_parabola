10b9cdb17f12b10fc70b0951d3294adc5bb23205
Diego <diegom.develops@gmail.com>
2022-02-14 19:03:10 -0400

    [ADD] function

---
+++ src/discriminant_of_a_parabola.py
@@ -0,0 +1,5 @@
+def discriminant_of_a_parabola():
+  a = int(input("Enter the value of a "))
+  b = int(input("Enter the value of b "))
+  c = int(input("Enter the value of c "))
+  return ((b)**2 - 4 * ((a) * (c)))
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from discriminant_of_a_parabola import *
+
+def main() -> None:
+  discriminant_of_a_parabola()
+
+if __name__ == "__main__":
+  main()
